3ca862428c19e838c035e1a14fded3a96415cc4d
angelica-pan <49770089+angelica-pan@users.noreply.github.com>
2020-05-08 09:47:21 -0700

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,284 +1,180 @@
 PennController.ResetPrefix(null);                       // Initiates PennController
 var showProgressBar = false;                            // Don't show progress bar
 
-// experimental and filler items have to be separated so that experimental items are displayed by counterbalancing group, but all filler items are displayed
-Sequence("instructions", shuffle(randomize("items"), randomize("fillers")))
+// Experimental and filler items are separated so that  all filler items are displayed, but experimental items are displayed by counterbalancing group.
+Sequence(randomize("practice"), "post-practice", "test_fillers", "exit", "send", "confirmation")
+
+//Sequence("consent","instructions","practice", shuffle(randomize("items"), randomize("fillers")), "exit", "send", "confirmation")
+//Sequence("consent","instructions","practice",  shuffle(randomize("test_items"), randomize("test_fillers")), "exit", "send", "confirmation")
+
+// Consent form
+newTrial("consent",
+    newHtml("consent_form", "UC-intro.html")
+        .settings.cssContainer({"width": "720px"})
+        .checkboxWarning("You must consent before continuing.")
+        .print()
+    ,
+    newButton("Click here to continue")
+        .center()
+        .css({"background-color":"cornflowerblue"})
+        .print()
+        .wait(
+            getHtml("consent_form").test.complete()
+            .failure(getHtml("consent_form").warn() )
+        )
+)
+
 
 // Instructions 
 newTrial("instructions",
-    defaultText
+    newHtml("instructions", "UC-instructions.html")
         .settings.cssContainer({"width": "600px"})
+        .print()
+    ,
+    newButton("Click here to continue")
+        .css({"background-color":"cornflowerblue"})
+    	.center()
+        .print()
+        .wait()
+)
+
+
+// Exit form 
+newTrial("exit",
+    newHtml("exit_form", "UC-exit.html")
+        .log()
+        .settings.cssContainer({"width": "720px"})
+        .inputWarning("Please enter your name and create a unique identifier.")
+        .print()
+    ,
+    newButton("Click here to finish the experiment.")
+        .css({"background-color":"cornflowerblue", "font-size": "150%", "font-weight": "bold"})
+        .size(500,50)
+        .center()
+        .print()
+        .wait(
+            getHtml("exit_form").test.complete()
+            .failure(getHtml("exit_form").warn() )
+        )
+)
+
+// End of experiment confirmation
+newTrial("confirmation",
+    newText("Thank you for participating! You may now exit the window.")
         .center()
         .print()
     ,
-	newText("<p> Welcome to the experiment! </p>")
+    newButton("void")
+        .wait()
+)
+
+
+// Trial item template (https://www.pcibex.net/forums/topic/mixing-fillers-and-items/#post-5200)
+customTrial = label => variable => newTrial( label ,
+    defaultText
+        .center()
     ,
-    newText("<p> You will be listening to sentences while looking at a cross (+) at the center of the screen. Please listen to each sentence carefully so that you understand it fully. </p>")
-	,
-    newText("<p> Before each sentence, you will be given a word to monitor for. While listening to the sentence, press the SPACEBAR as soon as you hear the word. The given word might not actually be in the sentence, so listen carefully! </p>")
-    ,
-    newText("<p> Some of the sentences will be followed by comprehension questions. Press the F or J key to select the answer on the left or the answer on the right, respectively. </p>")
-    ,
-    newText("<p> Press the spacebar to continue. </p>")
+    // Display target word 
+    newText("listen_for", "Listen for:")
+        .css({"width": "300px"})
         .italic()
+    ,
+    newText("target_word", variable.target)
+        .css({"font-size": "200%", "width": "300px"})        
+    ,
+    newText("next", "Press the spacebar to continue.")
+        .css({"width": "300px"})
+        .italic()
+    ,
+    newCanvas(300, 300)
+        .add(0, 100, getText("listen_for"))
+        .add(0, 150, getText("target_word"))
+        .add(0, 225, getText("next"))
+        .print()
     ,
     newKey(" ")
         .wait()
     ,
     clear()
     ,
-    newText("<p> Each trial will proceed as follows: </p>")
+    // Play audio stimulus and record reaction time
+    newImage("fixation_cross", "fixation_cross.png")
+        .size(300, 300)
     ,
-    newText("<p> <b> 1. Smiley face </b>: Rest as needed. Press the spacebar when you are ready to continue. </p>")
-    ,
-    newText("<p> <b> 2. Target word </b>: You will see a word to listen for. Press the spacebar when you are ready to continue. </p>")
-    ,
-    newText("<p> <b> 3. Cross (+) </b>: The sentence is playing -- listen carefully, and push the SPACEBAR if you hear the target word. </p>")
-    ,
-    newText("<p> <b> 4. Comprehension question </b>: Some of the sentences will be followed by a comprehension question. Respond by pressing the F or J key. </p>")
-    ,
-    newText("<p> Press the START button to continue. </p>")
-    	.italic()
-    ,
-    newButton("START")
-    	.center()
+    newCanvas(300, 300)
+        .add(0, 25, getImage("fixation_cross"))
         .print()
+    ,
+    newKey("reaction_time", " ")          
+        // logs all SPACEBAR presses (https://www.pcibex.net/forums/topic/cant-get-results-properly-recorded/)    
+        .log("all")                         
+    ,
+    newAudio("stimulus", variable.stimulus)
+        .play()
+        .wait() 
+    ,
+	clear()
+    ,
+    // Comprehension question (https://www.pcibex.net/forums/topic/conditional-trials/)
+    (variable.question?[newText("question", variable.question)
+        .cssContainer({"width": "600px", "font-size": "150%", "height": "50px"})
+    ,
+    newText("answer_1", variable.answer_1)
+        .cssContainer({"width": "300px"})
+    ,
+    newText("answer_2", variable.answer_2)
+        .cssContainer({"width": "300px"})
+    ,
+    newCanvas(600, 300)
+        .add(0, 150, getText("question"))
+        .add(0, 200, getText("answer_1"))
+        .add(300, 200, getText("answer_2"))
+        .print()
+    ,
+    newKey("FJ")
+        .wait()
+        .log()
+    ]:null)
+)
+.log("group",       variable.group)
+.log("item",        variable.item)
+.log("condition",   variable.condition)
+.log("target",      variable.target)
+.log("correct",    	variable.correct)
+
+
+// Practice items
+newTrial("post-practice",
+    newText("done", "That was it for the practice items!")
+        .center()
+        .bold()
+        .css({"width": "300px"})
+    ,
+    newText("continue", "Press the spacebar to start the experiment.")
+        .center()
+        .italic()
+        .css({"width": "300px"})
+    ,
+    newCanvas(300, 300)
+        .add(0, 150, getText("done"))
+        .add(0, 200, getText("continue"))
+        .print()
+    ,
+    newKey(" ")
         .wait()
 )
 
-// experimental items for testing
-AddTable( "test_items" ,
-    "group,item,cond2,stimulus,target,sentence,question,answer_1,answer_2,correct\n"+
-	"A,1,Clash.NoShift,SC_Clash_NoShift_1.wav,target_1_A,question_1_A,WHERE DID THE MEN STOP?,THE DOLLAR STORE,THE CONSTRUCTION SITE,answer_1\n"+
-	"B,1,Clash.Shift,SC_Clash_Shift_1.wav,target_1_B,question_1_B,WHERE DID THE MEN STOP?,THE DOLLAR STORE,THE CONSTRUCTION SITE,answer_1\n"+
-	"C,1,NoClash.NoShift,SC_NoClash_NoShift_1.wav,target_1_C,question_1_C,WHERE DID THE MEN STOP?,THE DOLLAR STORE,THE CONSTRUCTION SITE,answer_1\n"+
-	"D,1,NoClash.Shift,SC_NoClash_Shift_1.wav,target_1_D,question_1_D,WHERE DID THE MEN STOP?,THE DOLLAR STORE,THE CONSTRUCTION SITE,answer_1\n"+
-	"B,2,Clash.NoShift,SC_Clash_NoShift_2.wav,target_2_B,question_2_B,Press the F or J key to continue.,,,\n"+
-	"C,2,Clash.Shift,SC_Clash_Shift_2.wav,target_2_C,question_2_C,Press the F or J key to continue.,,,\n"+
-	"A,2,NoClash.NoShift,SC_NoClash_NoShift_2.wav,target_2_A,question_2_A,Press the F or J key to continue.,,,\n"+
-	"D,2,NoClash.Shift,SC_NoClash_Shift_2.wav,target_2_D,question_2_D,Press the F or J key to continue.,,,\n"+
-	"C,3,Clash.NoShift,SC_Clash_NoShift_3.wav,target_3_C,question_3_C,WAS THE MEAL EXPENSIVE?,YES,NO,answer_1\n"+
-	"D,3,Clash.Shift,SC_Clash_Shift_3.wav,target_3_D,question_3_D,WAS THE MEAL EXPENSIVE?,YES,NO,answer_1\n"+
-	"B,3,NoClash.NoShift,SC_NoClash_NoShift_3.wav,target_3_B,question_3_B,WAS THE MEAL EXPENSIVE?,YES,NO,answer_1\n"+
-	"A,3,NoClash.Shift,SC_NoClash_Shift_3.wav,target_3_A,3_A,WAS THE MEAL EXPENSIVE?,YES,NO,answer_1\n"+
-	"D,4,Clash.NoShift,SC_Clash_NoShift_4.wav,target_4_D,question_4_D,WHAT DID THE DOGS DO?,GROWL,HOWL,answer_2\n"+
-	"A,4,Clash.Shift,SC_Clash_Shift_4.wav,target_4_A,question_4_A,WHAT DID THE DOGS DO?,GROWL,HOWL,answer_2\n"+
-	"C,4,NoClash.NoShift,SC_NoClash_NoShift_4.wav,target_4_C,question_4_C,WHAT DID THE DOGS DO?,GROWL,HOWL,answer_2\n"+
-	"B,4,NoClash.Shift,SC_NoClash_Shift_4.wav,target_4_B,question_4_B,WHAT DID THE DOGS DO?,GROWL,HOWL,answer_2"
-);
+// Practice items
+Template("practice.csv", customTrial("practice"))
 
-// filler items for testing
-AddTable( "test_fillers" ,
-	"item,cond2,stimulus,target,sentence,question,answer_1,answer_2,correct\n+"+
-	"1,filler,filler.wav,filler_1,,QUESTION?,YES,NO,answer_1\n+"+
-	"2,filler,filler.wav,filler_2,,QUESTION?,YES,NO,answer_1\n+"+
-	"3,filler,filler.wav,filler_3,,Press the F or J key to continue.,,,\n+"+
-	"4,filler,filler.wav,filler_4,,Press the F or J key to continue.,,,"
-);
+// Experimental items
+Template("test_items.csv", customTrial("test_items"))
+Template("items.csv", customTrial("items"))
 
-// Experimental item trial 
-PennController.Template("items.csv",	// use "test_items" to test
-    variable => PennController("items",
-        defaultText
-            .center()
-        ,
-        // Rest screen in between trials (press space to continue)
-        newImage("smiley", "smiley.png")
-            .size(300, 300)                             // Resize smiley.png to 300x300px
-        ,
-        newCanvas(300, 300)                             // Every image or text will be displayed in a 300x300px Canvas.
-            .add(0, 0, getImage("smiley"))                  // This helps maintain consistent horizontal and vertical spacing.
-            .print()
-        ,
-        newText("<p> Press the spacebar when you are ready to start the trial. </p>")
-            .italic()
-            .print()
-        ,
-        newKey(" ")
-            .wait()
-        ,
-        clear()
-        ,
-        
-        // Display target word (press space to continue)
-        newText("target_word", variable.target)
-            .settings.css({"font-size": "200%"})        // Make target word font larger so that it stands out.
-            .print()
-        ,
-        newCanvas(300, 300)
-            .add("center at 50%", "center at 50%", getText("target_word"))
-            .print()
-        ,
-        newText("<p> Press the spacebar to continue. </p>")
-            .italic()
-            .print()
-        ,
-        newKey(" ")
-            .wait()
-        ,
-        clear()
-        ,
-        
-        // Play audio stimulus and record reaction time
-        newImage("fixation_cross", "fixation_cross.png")
-            .size(300, 300)
-        ,
-        newCanvas(300, 300)
-            .add(0, 0, getImage("fixation_cross"))
-            .print()
-        ,
-        newKey("reaction_time", " ")                    // Cannot use .log() or .log("first"); see <https://www.pcibex.net/forums/topic/cant-get-results-properly-recorded/>
-            .log("all")                                 // Log the reaction time
-        ,
-        newAudio("stimulus", variable.stimulus)
-            .play()
-            .wait() 
-        ,
-    	clear()
-        ,
-        newText("question", variable.question)
-            .settings.cssContainer({"width": "600px", "font-size": "150%", "height": "50px"})
-            .center()
-        ,
-        newText("answer_1", variable.answer_1)
-            .settings.cssContainer({"width": "300px"})
-            .center()
-        ,
-        newText("answer_2", variable.answer_2)
-            .settings.cssContainer({"width": "300px"})
-            .center()
-        ,
-        newCanvas(600, 300)
-            .add(0, 150, getText("question"))
-            .add(0, 200, getText("answer_1"))
-            .add(300, 200, getText("answer_2"))
-            .print()
-        ,
-        newKey("FJ")
-            .wait()
-            .log()
-    )
-    .log("group",       variable.group)
-    .log("item",        variable.item)
-    .log("condition",   variable.cond2)
-    .log("target",      variable.target)
-    .log("correct",    	variable.correct)
+// Filler items
+Template("test_fillers.csv", customTrial("test_fillers"))
+Template("fillers.csv", customTrial("fillers"))
 
-);
+// Send results
+PennController.SendResults("send");
 
-// Filler item trial 
-PennController.Template("fillers.csv", // use "test_fillers" to test
-    variable => PennController("fillers",
-        defaultText
-            .center()
-        ,
-        // Rest screen in between trials (press space to continue)
-        newImage("smiley", "smiley.png")
-            .size(300, 300)                             // Resize smiley.png to 300x300px
-        ,
-        newCanvas(300, 300)                             // Every image or text will be displayed in a 300x300px Canvas.
-            .add(0, 0, getImage("smiley"))                  // This helps maintain consistent horizontal and vertical spacing.
-            .print()
-        ,
-        newText("<p> Press the spacebar when you are ready to start the trial. </p>")
-            .italic()
-            .print()
-        ,
-        newKey(" ")
-            .wait()
-        ,
-        clear()
-        ,
-        
-        // Display target word (press space to continue)
-        newText("target_word", variable.target)
-            .settings.css({"font-size": "200%"})        // Make target word font larger so that it stands out.
-            .print()
-        ,
-        newCanvas(300, 300)
-            .add("center at 50%", "center at 50%", getText("target_word"))
-            .print()
-        ,
-        newText("continue", "<p> Press the spacebar to continue. </p>")
-            .italic()
-            .print()
-        ,
-        newKey(" ")
-            .wait()
-        ,
-        clear()
-        ,
-        
-        // Play audio stimulus and record reaction time
-        newImage("fixation_cross", "fixation_cross.png")
-            .size(300, 300)
-        ,
-        newCanvas(300, 300)
-            .add(0, 0, getImage("fixation_cross"))
-            .print()
-        ,
-        newKey("reaction_time", " ")                    // Cannot use .log() or .log("first"); see <https://www.pcibex.net/forums/topic/cant-get-results-properly-recorded/>
-            .log("all")                                 // Log the reaction time
-        ,
-        newAudio("stimulus", variable.stimulus)
-            .play()
-            .wait() 
-    	,
-    	clear()
-        ,
-        newText("question", variable.question)
-            .settings.cssContainer({"width": "600px", "font-size": "150%", "height": "50px"})
-            .center()
-        ,
-        newText("answer_1", variable.answer_1)
-            .settings.cssContainer({"width": "300px"})
-            .center()
-        ,
-        newText("answer_2", variable.answer_2)
-            .settings.cssContainer({"width": "300px"})
-            .center()
-        ,
-        newCanvas(600, 300)
-            .add(0, 150, getText("question"))
-            .add(0, 200, getText("answer_1"))
-            .add(300, 200, getText("answer_2"))
-            .print()
-        ,
-        newKey("FJ")
-            .wait()
-            .log()
-    )
-    .log("group",       variable.group)
-    .log("item",        variable.item)
-    .log("condition",   variable.cond2)
-    .log("target",      variable.target)
-    .log("correct",    	variable.correct)
-
-);
-
-// Comprehension question code (saved as a backup)
-newTrial(
-        clear()
-        ,
-        newText("question", variable.question)
-            .settings.cssContainer({border: "solid 1px purple", "width": "600px", "font-size": "150%", "height": "50px"})
-            .center()
-        ,
-        newText("answer_1", variable.answer_1)
-            .settings.cssContainer({border: "solid 1px black", "width": "300px"})
-            .center()
-        ,
-        newText("answer_2", variable.answer_2)
-            .settings.cssContainer({border: "solid 1px red", "width": "300px"})
-            .center()
-        ,
-        newCanvas(600, 300)
-            .add(0, 100, getText("question"))
-            .add(0, 150, getText("answer_1"))
-            .add(300, 150, getText("answer_2"))
-            .print()
-        ,
-        newKey("FJ")
-            .wait()
-            .log()
-)
