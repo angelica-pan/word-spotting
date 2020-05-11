f61d1bb3191c05a9243f649efaf6f6f90d6096cc
angelica-pan <49770089+angelica-pan@users.noreply.github.com>
2020-05-11 15:43:47 -0700

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,11 +1,92 @@
 PennController.ResetPrefix(null);                       // Initiates PennController
 var showProgressBar = false;                            // Don't show progress bar
 
-// Experimental and filler items are separated so that  all filler items are displayed, but experimental items are displayed by counterbalancing group.
-Sequence(randomize("practice"), "post-practice", "test_fillers", "exit", "send", "confirmation")
+// Test items/fillers (4 of each).
+//Sequence("consent", "instructions", randomize("practice"), "post-practice", shuffle(randomize("test_items"), randomize("test_fillers")), "exit", "send", "confirmation")
 
-//Sequence("consent","instructions","practice", shuffle(randomize("items"), randomize("fillers")), "exit", "send", "confirmation")
-//Sequence("consent","instructions","practice",  shuffle(randomize("test_items"), randomize("test_fillers")), "exit", "send", "confirmation")
+// Full list of items/fillers.
+//Sequence("consent", "instructions", randomize("practice"), "post-practice", shuffle(randomize("items"), randomize("fillers")), "exit", "send", "confirmation")
+
+Sequence("sound_check", "instructions")
+
+customButton = text  => 
+    newButton(text)
+        .center()
+        .css({"background-color":"cornflowerblue"})
+        .print()
+        .wait()
+        
+// sound check
+newTrial("sound_check",
+    defaultText
+        .center()
+    ,
+    // sound check pt. 1: participant hears looping tones
+    newText("<p> Please check that your headphones or computer speakers are working by pressing the play button below.</p>")
+        .print()
+    ,
+    newAudio("test_tone_audio", "sound_check_tone.wav")
+        .center()
+        .cssContainer({"height" : "75px"})
+        .print()
+        .wait()
+    ,
+    getAudio("test_tone_audio")
+        .play("loop")
+    ,
+    newText("<p> If you cannot hear the audio, please adjust your headphones or speakers until the sound file is audible. </p>")
+        .print()
+    ,
+    customButton("If you can hear the audio, click here to continue")
+    ,
+    clear()
+    ,
+    // sound check pt. 2: participant hears and types a sentence
+    newText("<p> As an additional sound check, please listen to the following audio file, and then type the sentence that you've just heard into the text box. </p>")
+        .print()
+    ,
+    newAudio("test_sentence_audio", "sound_check_sentence.wav")
+        .center()
+        .cssContainer({"height" : "75px"})
+        .print()
+    ,
+    newTextInput("test_sentence", "")
+        .size(400, 25)
+        .cssContainer({"height" : "50px"})
+        .center()
+        .print()
+    ,
+    newText("incorrect", "Try again...")
+        .css({"color": "red"})
+    ,
+    newText("correct", "That's right!")
+        .css({"color": "green"})
+    ,
+    newButton("validate", "Validate")
+        .center()
+        .css({"background-color":"cornflowerblue"})
+        .print()
+        .wait(
+            getTextInput("test_sentence")
+                // regular expression that the [text input box] should match
+                .test.text(/It was my niece who first taught me how to play video games/i)
+                    // if [text input] doesn't match, print [incorrect] error message
+                    .failure(getText("incorrect").print())
+                    // if [text input] matches, print [correct] error message
+                    .success(
+                        getText("incorrect")
+                            .remove()
+                        ,
+                        getText("correct")
+                            .print()
+                    )
+            )
+    ,
+    getButton("validate")
+        .remove()
+    ,
+    customButton("Click here to continue")
+)
 
 // Consent form
 newTrial("consent",
@@ -28,16 +109,32 @@ newTrial("consent",
 // Instructions 
 newTrial("instructions",
     newHtml("instructions", "UC-instructions.html")
-        .settings.cssContainer({"width": "600px"})
+        .settings.cssContainer({"width": "720px"})
         .print()
     ,
-    newButton("Click here to continue")
-        .css({"background-color":"cornflowerblue"})
-    	.center()
-        .print()
-        .wait()
+    customButton("Click here to continue")
 )
 
+// Post-practice items
+newTrial("post-practice",
+    newText("done", "That was it for the practice items!")
+        .center()
+        .bold()
+        .css({"width": "300px"})
+    ,
+    newText("continue", "Press the spacebar to start the experiment.")
+        .center()
+        .italic()
+        .css({"width": "300px"})
+    ,
+    newCanvas(300, 300)
+        .add(0, 150, getText("done"))
+        .add(0, 200, getText("continue"))
+        .print()
+    ,
+    newKey(" ")
+        .wait()
+)
 
 // Exit form 
 newTrial("exit",
@@ -68,6 +165,7 @@ newTrial("confirmation",
         .wait()
 )
 
+    
 
 // Trial item template (https://www.pcibex.net/forums/topic/mixing-fillers-and-items/#post-5200)
 customTrial = label => variable => newTrial( label ,
@@ -142,28 +240,6 @@ customTrial = label => variable => newTrial( label ,
 .log("target",      variable.target)
 .log("correct",    	variable.correct)
 
-
-// Practice items
-newTrial("post-practice",
-    newText("done", "That was it for the practice items!")
-        .center()
-        .bold()
-        .css({"width": "300px"})
-    ,
-    newText("continue", "Press the spacebar to start the experiment.")
-        .center()
-        .italic()
-        .css({"width": "300px"})
-    ,
-    newCanvas(300, 300)
-        .add(0, 150, getText("done"))
-        .add(0, 200, getText("continue"))
-        .print()
-    ,
-    newKey(" ")
-        .wait()
-)
-
 // Practice items
 Template("practice.csv", customTrial("practice"))
 
@@ -177,4 +253,3 @@ Template("fillers.csv", customTrial("fillers"))
 
 // Send results
 PennController.SendResults("send");
-
